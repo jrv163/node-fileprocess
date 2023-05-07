b1f23162bf897b670ebd05f1d0beab80de129184
Jamer Rodríguez <jrv163@hotmail.com>
2023-05-07 14:15:32 -0500

    Primer Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instalación
+
+Ejecutar el comando ````npm install``` para instalar los comandos de Node
+
+## Ejecución
+
+Ejecutar el comando ```node app.js``` para visualizar la aplicación
---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express')
+
+const app = express();
+const port = process.env.PORT;
+
+app.use( express.static('public') );
+
+
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/404.html')
+  })
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
